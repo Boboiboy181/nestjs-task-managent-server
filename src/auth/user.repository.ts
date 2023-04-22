3b3f0548bf01369bcdb48560f7b808204629bad2
Gia Hai <haidao549@gmail.com>
2023-04-22 15:49:05 +0700

    securely story password with hashing method

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,12 @@
 import { Repository, DataSource } from 'typeorm';
 import { User } from './user.entity';
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
@@ -10,10 +15,21 @@ export class UserRepository extends Repository<User> {
   }
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({
-      username,
-      password,
-    });
-    await this.save(user);
+
+    // hash
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
